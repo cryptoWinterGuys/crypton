ae266138c787b3c1cb10d72274d9eb9d153faad7
James Isaacs <jkifao@gmail.com>
2022-11-03 22:58:04 -0400

    updates to locations

--- javascript/crypto-locations.js
+++ javascript/crypto-locations.js
@@ -1,27 +1,25 @@
-const map = L.map('map').setView([51.505, -0.09], 2);
+const map = L.map("map").setView([51.505, -0.09], 2);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-
 async function getMapData() {
-    try {
-        const results = await fetch("https://coinmap.org/api/v1/venues/?limit=300");
-        console.log(results);
-        const data = await results.json();
-        console.log(data);
-        data.venues.forEach(venue => {
-            const marker = L.marker([venue.lat, venue.lon]).addTo(map);
-            marker.bindPopup(venue.name + ": " + venue.category);
-        });
-
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		const results = await fetch(
+			"https://cors-anywhere.herokuapp.com/https://coinmap.org/api/v1/venues/?limit=300"
+		);
+		console.log(results);
+		const data = await results.json();
+		console.log(data);
+		data.venues.forEach((venue) => {
+			const marker = L.marker([venue.lat, venue.lon]).addTo(map);
+			marker.bindPopup(venue.name + ": " + venue.category);
+		});
+	} catch (error) {
+		console.log(error);
+	}
 }
 getMapData();
 // loadMaps.onclick = getMapData;
-
-
